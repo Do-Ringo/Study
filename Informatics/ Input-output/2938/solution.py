d495a11fee2a4f7c89045fcc1518735d90475e5f
Dasha <smalkodarina@gmail.com>
2023-07-16 21:47:39 +0300

    :)

---
+++ Informatics/ Input-output/2938/solution.py
@@ -0,0 +1,7 @@
+n: str = input()
+k: str = input()
+N: int = int(n)
+K: int = int(k)
+A: float = K/N
+a: int = int(A)
+print(a)
---
+++ Informatics/ Input-output/2939/solution.py
@@ -0,0 +1,10 @@
+n: str = input()
+k: str = input()
+N: int = int(n)
+K: int = int(k)
+A: float = K/N
+a: int = int(A)
+d: float = A-a
+D: float = d * 3
+G: int = int(D)
+print(G)
